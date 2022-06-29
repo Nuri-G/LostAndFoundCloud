149f231f95aff707ded3226bb9f92a4e47f1a57c
Nurivan Gomez <nurivan@fb.com>
2022-06-29 15:36:44 -0700

    Added item match checking function.

--- main.js
+++ main.js
@@ -12,6 +12,67 @@ const LostItem = Parse.Object.extend("LostItem");
 const FoundItem = Parse.Object.extend("FoundItem");
 const Match = Parse.Object.extend("Match");
 
+//Returns value from 0 to 1 depending on how close to 0 the distance is.
+function locationSimilarity(item, otherItem) {
+    const MAX_DISTANCE = 50.0;
+    let distanceMiles = item.get(KEY_ITEM_LOCATION).milesTo(otherItem.get(KEY_ITEM_LOCATION));
+
+    if(distanceMiles > MAX_DISTANCE) {
+        return 0;
+    } else {
+        return (MAX_DISTANCE - distanceMiles) / MAX_DISTANCE;
+    }
+}
+
+function nameSimilarity(item, otherItem) {
+    let itemName = item.get(KEY_ITEM_NAME).toLowerCase();
+    let otherName = otherItem.get(KEY_ITEM_NAME).toLowerCase();
+
+    let longerLength = Math.max(itemName.length, otherName.length);
+    let similarity = levenshtein.get(itemName, otherName);
+
+    return (longerLength - similarity) / longerLength;
+}
+
+function timeSimilarity(item, otherItem) {
+    let lostItem;
+    let foundItem;
+    if(item.className == 'LostItem') {
+        lostItem = item
+        foundItem = otherItem;
+    } else {
+        lostItem = otherItem;
+        foundItem = item;
+    }
+
+    let lostDate = lostItem.get(KEY_TIME_LOST);
+    let foundDate = foundItem.get(KEY_TIME_FOUND);
+
+    const oneDay = 24 * 60 * 60 * 1000;
+
+    const daysBetween = Math.round(Math.abs((lostDate - foundDate) / oneDay));
+
+    const MAX_DAYS_AFTER = 14;
+    const MAX_DAYS_BEFORE = 1;
+
+    if(daysBetween > -MAX_DAYS_BEFORE && daysBetween < MAX_DAYS_AFTER) {
+        return (MAX_DAYS_AFTER - daysBetween) / MAX_DAYS_AFTER;
+    }
+    return 0;
+}
+
+function checkItemMatch(item, otherItem) {
+    const NAME_SIMILARITY_WEIGHT = 0.4;
+    const LOCATION_SIMILARITY_WEIGHT = 0.3;
+    const TIME_SIMILARITY_WEIGHT = 0.3;
+
+    let nameSim = nameSimilarity(item, otherItem);
+    let locationSim = locationSimilarity(item, otherItem);
+    let timeSim = timeSimilarity(item, otherItem);
+
+    return NAME_SIMILARITY_WEIGHT * nameSim + LOCATION_SIMILARITY_WEIGHT * locationSim + TIME_SIMILARITY_WEIGHT * timeSim;
+}
+
 async function setMatches(item) {
     // Matches will be set here.
 }
